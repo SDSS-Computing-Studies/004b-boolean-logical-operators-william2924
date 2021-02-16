904420efc67444b9f924da3f9213b4750e1ca464
william2924 <78515221+william2924@users.noreply.github.com>
2021-02-15 18:38:10 -0800

    all

--- problem1.py
+++ problem1.py
@@ -1,21 +1,7 @@
-"""
-Problem 1
-Ask the user to enter a number.
-The number is considered "frue" if it is
-divisible by 6, but not divisible by 8.
-State whether the number is "frue" 
-(2 marks)
-
-Inputs:
-a number
-
-Outputs:
-xx is frue
-xx is not frue
-
-example:
-Enter a number: 48
-48 is not frue
-"""
-
 #! python3
+
+number = int(input())
+if number % 6 == 0 and number % 8 != 0:
+    print(str(number) + " is frue")
+else:
+    print(str(number) + " is not frue")
---
+++ task1.py
@@ -0,0 +1,26 @@
+
+#! python3
+"""
+The Earth maintains an orbit where it's closest distance to  
+the sun is 0.9759 AU and it's furthest distance to the sun is 
+1.016 AU. 
+Ask the user to enter a number. 
+Tell them if the number is between 0.9759 and 1.016.
+(2 points)
+
+Inputs:
+a number (float)
+
+Outputs:
+That is within normal Earth orbit.
+That is not within normal Earth orbit.
+"""
+
+number = float(input())
+a = 0.9759 
+b = 1.016
+
+if a < number < b:
+    print("That is within normal Earth orbit.")
+else:
+    print("That is not within normal Earth orbit.")
